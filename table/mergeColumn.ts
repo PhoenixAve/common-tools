6006498c5315b23ac6e2cfc3bdd927b4afd86056
PhoenixAve <15565999182@163.com>
2024-01-05 16:27:45 +0800

    Update mergeColumn.ts

--- table/mergeColumn.ts
+++ table/mergeColumn.ts
@@ -102,3 +102,61 @@ export const getRowSpan = (prop, rowIndex, rowSpanMap) => {
         return { colspan: 0, rowspan: 0 };
     }
 };
+
+/**
+ * 获取文字宽度
+ * @param text
+ * @param style
+ * @returns
+ */
+export const getTextWidth = (text: string, style?: { fontStyle: string, fontSize: number, fontWeight: string, fontFamily: string }) => {
+    style = Object.assign({ fontSize: 12, fontStyle: 'normal', fontWeight: 'normal', fontFamily: 'PingFang SC,serif' }, style);
+    // 获取Canvas元素
+    function createCanvas(): { canvas: HTMLCanvasElement, context: CanvasRenderingContext2D } {
+        const canvas = document.createElement('canvas');
+        canvas.width = 500;
+        canvas.style.position = 'absolute';
+        canvas.style.zIndex = '-1';
+        canvas.style.left = '-9999px';
+        canvas.setAttribute('id', 'myCanvas');
+        document.body.appendChild(canvas);
+        return {
+            canvas,
+            context: canvas.getContext('2d')
+        };
+    }
+    if (!getTextWidth['context']) {
+        const { canvas, context } = createCanvas();
+        getTextWidth['canvas'] = canvas;
+        getTextWidth['context'] = context;
+    }
+    const canvas: HTMLCanvasElement = getTextWidth['canvas'];
+    const context: CanvasRenderingContext2D = getTextWidth['context'];
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    // 设置字体样式
+    context.font = `${style.fontStyle} ${style.fontWeight} ${style.fontSize}px ${style.fontFamily}`;
+    // 输出文本宽度
+    return Math.ceil(context.measureText(text).width);
+};
+
+/**
+ * 获取某些列的值的最大文本宽度
+ * @param rows
+ * @param columns
+ * @param getExtraWidth 额外需要增加的宽度，例如小图标
+ * @returns
+ */
+export const getColumnValueMaxWidth = (rows: TableRowVo[], columns: string[], getExtraWidth?: (row: TableRowVo, column: string) => number) => {
+    const result = {};
+    rows.forEach(row => {
+        columns.forEach(column => {
+            const text: string | number = row.value[column].value;
+            let extraWidth = 0;
+            if (typeof getExtraWidth === 'function') {
+                extraWidth = toSafeInteger(getExtraWidth(row, column));
+            }
+            result[column] = Math.max(result[column] || 0, getTextWidth(text.toString()) + extraWidth);
+        });
+    });
+    return result;
+};
