de5da0ed915591c4a24b185c766f798284e26ca8
PhoenixAve <15565999182@163.com>
2024-05-13 10:18:04 +0800

    Update copy.ts

--- tools/copy.ts
+++ tools/copy.ts
@@ -49,3 +49,78 @@ export const copyDirective = {
         el.removeEventListener('click', el['_copyHandler']);
     },
 };
+
+export default {
+    async copyImg(url: string, onSuccess, onError): Promise<void> {
+        try {
+            const base64Url: string = await this.urlToBase64(url);
+            const data = await fetch(base64Url);
+            const blob = await data.blob();
+            const item = new window.ClipboardItem({
+                [blob.type]: blob,
+            });
+            const clipboard = navigator.clipboard;
+            if (clipboard) {
+                await clipboard.write([item]);
+                onSuccess();
+            } else {
+                throw new Error("浏览器不支持clipboard，请点击图片右键使用复制图片功能或者使用谷歌浏览器");
+            }
+        } catch (error) {
+            onError(error);
+        }
+    },
+    async copy(url: string, onSuccess, onError): Promise<void> {
+        const clipboard = navigator.clipboard;
+        if (clipboard) {
+            await clipboard.writeText(url);
+            onSuccess();
+        } else {
+            this.execCommandCopy(url, onSuccess, onError);
+        }
+    },
+    execCommandCopy(url: string, onSuccess, onError): void {
+        try {
+            // 创建一个input框
+            const hiddenInput: HTMLInputElement = document.createElement('input');
+            // 赋值
+            hiddenInput.setAttribute('value', url);
+            // 把input添加到body中
+            document.body.appendChild(hiddenInput);
+            // 选中input框，取值复制
+            hiddenInput.select();
+            document.execCommand('copy', false);
+            // 删除input框
+            document.body.removeChild(hiddenInput);
+            onSuccess();
+        } catch (error) {
+            onError();
+        }
+    },
+    async downloadImg(url: string, fileName: string): Promise<void> {
+        const base64Url: string = await this.urlToBase64(url);
+        const a = document.createElement('a');
+        const event = new MouseEvent('click');
+        a.download = fileName;
+        a.href = base64Url;
+        a.dispatchEvent(event);
+    },
+    urlToBase64(url: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            const image = new Image();
+            image.onload = function () {
+                const canvas = document.createElement('canvas');
+                canvas.width = image.width;
+                canvas.height = image.height;
+                (canvas.getContext('2d') as CanvasRenderingContext2D).drawImage(image, 0, 0);
+                const result = canvas.toDataURL('image/png');
+                resolve(result);
+            };
+            image.setAttribute("crossOrigin", 'Anonymous');
+            image.src = url;
+            image.onerror = () => {
+                reject(new Error('图片流异常，有可能是图片地址无效'));
+            };
+        });
+    }
+};
